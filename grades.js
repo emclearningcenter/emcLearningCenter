3fea647df16e2938ca158e9ffee29e9353ec1fa6
Samia <nislam17@gmail.com>
2014-02-16 13:51:58 -0500

    updates

---
+++ grades.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $("#one").click(function(){
+    $("#onePanel").slideToggle();
+});
+	$("#two").click(function(){
+   	 $("#twoPanel").slideToggle();
+});
+	$("#three").click(function(){
+   	 $("#threePanel").slideToggle();
+});
+	$("#four").click(function(){
+   	 $("#fourPanel").slideToggle();
+});
+	$("#five").click(function(){
+   	 $("#fivePanel").slideToggle();
+});
+	$("#six").click(function(){
+   	 $("#sixPanel").slideToggle();
+});
+
+
+});
+
